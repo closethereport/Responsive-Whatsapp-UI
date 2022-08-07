2c1597a49f1281a1536bac2541bd4dafd879fea0
closethereport <closethereport@gmail.com>
2022-08-07 08:17:14 +0300

    Displaying Web Contact List

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is web'),
-      ),
-    );
+    return Scaffold(
+        body: Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Web Profile Bar
+                //Web Search Bar
+                ContactsList(),
+              ],
+            ),
+          ),
+        ),
+        //Web Screen
+        Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/backgroundImage.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
